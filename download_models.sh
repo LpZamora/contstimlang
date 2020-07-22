fab9bc195b1a5853529570fb149958b4f4d1daf2
msieg <37848286+msieg@users.noreply.github.com>
2020-07-21 21:02:57 -0400

    Update download_models.sh
    Former-commit-id: 86bef72df509d04ce477f81b334062f5d2bf1546

--- download_models.sh
+++ download_models.sh
@@ -1,4 +1,13 @@
+gdown https://drive.google.com/uc?id=1ZMpOAaR9EUBEBK71W3i7Ozn8NubStcg2
+
+gdown https://drive.google.com/uc?id=1YlQ7b18HjnEwlORAI8CYePh4tVCLOygq
+
 gdown https://drive.google.com/uc?id=1uIOr0c--qsZvMymk-fBGYAc4sF5DS73K
 
 gdown https://drive.google.com/uc?id=1BpqE1bFGHQ0E3Rk_1zny13PyPM38Xfg6
 
+gdown https://drive.google.com/uc?id=1gdm3JWG4vb7aSucFBq3Bovf4aqR03c4h
+
+gdown https://drive.google.com/uc?id=1VeEMSNzP2k8pgwkl0y2amNjvyHHUBSMU
+
+gdown https://drive.google.com/uc?id=10YCSrePs3aBDwMRw9g-Lq-9Cfzvy1ECY
