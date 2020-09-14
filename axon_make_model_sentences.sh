46a699b3fc40e0150385d3bc32888e3a78ad4df3
Tal Golan <tg2632@axon.rc.zi.columbia.edu>
2020-09-14 13:33:22 -0400

    adding axon script
    Former-commit-id: e3acb2e6315adeea1a79874e9f169820359d7d9c

---
+++ axon_make_model_sentences.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#
+#
+#SBATCH --account=nklab
+#SBATCH --job-name=make_model_sent    # The job name.
+#SBATCH --gres=gpu:1
+#SBATCH --error="slurm-%A_%a.err"
+#SBATCH --time=4-0:0
+
+module load anaconda3-2019.03 
+
+cd /scratch/nklab/projects/contstimlang/contstim
+
+. activate contstimlang
+
+python -u make_model_sentences.py
+echo "python script terminated"
+
+# End of script
