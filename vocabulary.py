e40045f804d28ae41bdfa285dc6dfd4b14c501ef
Tal Golan <golantl@gmail.com>
2020-12-16 20:29:33 -0500

    fixing the previous commit
    Former-commit-id: 054cb105656cf8178afe362478db60ad3d337bdf

---
+++ vocabulary.py
@@ -0,0 +1,14 @@
+import pickle
+
+# load vocabulary and word probabilities
+with open('vocab_low.pkl', 'rb') as file:
+    vocab_low=pickle.load(file) 
+    
+with open('vocab_low_freqs.pkl', 'rb') as file:
+    vocab_low_freqs=pickle.load(file) 
+
+with open('vocab_cap.pkl', 'rb') as file:
+    vocab_cap=pickle.load(file) 
+    
+with open('vocab_cap_freqs.pkl', 'rb') as file:
+    vocab_cap_freqs=pickle.load(file)
