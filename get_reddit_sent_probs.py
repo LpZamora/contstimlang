7b3dbad14c8c52869f68c18482ca44b401661106
msieg <37848286+msieg@users.noreply.github.com>
2021-05-26 13:38:15 -0400

    Add files via upload
    Former-commit-id: 9834a9311571c841ebcf5ee8451ae74f04668fdc

---
+++ get_reddit_sent_probs.py
@@ -0,0 +1,35 @@
+import pickle
+import numpy as np
+import random
+import math
+import argparse
+
+from lstm_class import RNNLM
+from bilstm_class import RNNLM_bilstm
+from rnn_class import RNNModel
+from model_functions import model_factory
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--model",required=True)
+args = parser.parse_args()
+
+model=args.model
+
+model1=model_factory(model,0)
+
+file=open('sents_reddit_natural_May2021_filtered.txt','r')
+sents=file.read()
+sents=sents.split('\n')
+
+probs=[]
+for si,sent in enumerate(sents):
+    
+    if si%10==0:
+        print(si)
+        
+    prob=model1.sent_prob(sent)
+    probs.append(prob)
+    
+probs=np.array(probs)
+
+np.save('sents_reddit_natural_May2021_filtered_probs_'+model+'.npy',probs)
