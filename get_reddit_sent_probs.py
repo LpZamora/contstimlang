b6c525d849b391e7d1a787efab2959485a918bc8
Tal Golan <golantl@gmail.com>
2021-06-03 23:10:08 -0400

    preparing for June2021 natural controversial get_prob
    Former-commit-id: 303fa7df69eabfb69a4087ae7315d777fedb939c [formerly c6aa7040310e13317584446a2ee036752f378692]
    Former-commit-id: 11e7c3834f1cb19c41ba1f59bedf5c1204a7a42d

--- get_reddit_sent_probs.py
+++ get_reddit_sent_probs.py
@@ -13,13 +13,15 @@ from model_functions import model_factory
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--model",required=True)
+parser.add_argument("--gpu",default=0)
+
 args = parser.parse_args()
 
 model=args.model
 
-model1=model_factory(model,0)
+model1=model_factory(model,args.gpu)
 
-file=open('sents_reddit_natural_May2021_filtered.txt','r')
+file=open('natural_sentence_probabilities/sents_reddit_natural_June2021_filtered.txt','r')
 sents=file.read()
 sents=sents.split('\n')
 
@@ -30,4 +32,4 @@ for sent in tqdm(sents):
 
 probs=np.array(probs)
 
-np.save('sents_reddit_natural_May2021_filtered_probs_'+model+'.npy',probs)
+np.save('sents_reddit_natural_June2021_filtered_probs_'+model+'.npy',probs)
