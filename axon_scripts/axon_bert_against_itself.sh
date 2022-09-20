7562e9f196465796250db9f9d23be5b62a2c867b
Tal Golan <golantl@gmail.com>
2022-09-20 17:58:05 -0400

    axon script bugfixes

--- axon_scripts/axon_bert_against_itself.sh
+++ axon_scripts/axon_bert_against_itself.sh
@@ -7,14 +7,14 @@
 #SBATCH --mem=16G
 #SBATCH --error="slurm-%A_%a.err"
 #SBATCH --time=24:00:00
-
+#SBATCH --exclude=ax11,ax12,ax13
 module load anaconda3-2019.03
 
-cd /scratch/nklab/projects/contstimlang/contstim
+cd /scratch/nklab/projects/contstimlang/contstimlang
 
 conda activate contstimlang
 
-python -u bert_against_itself.py
+python -u batch_bert_against_itself.py
 echo "python script terminated"
 
 # End of script
